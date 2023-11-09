89abe576b5335ea5a5842f766c45a8ac13c82b87
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-11-09 14:48:06 -0800

    just added what's required but testing failed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,7 @@ class FileStorage:
         if obj is not None:
             key = f'{obj.__class__.__name__}.{obj.id}'
             FileStorage.__objects.pop(key, None)
+
+    def close(self):
+        '''Close method documentation'''
+        self.reload()
