627d96c34b621e365b16c7ee6e3870a0b5bfb96a
Svitlana PAvlovska <lanapavlovska90@gmail.com>
2023-11-09 14:28:16 -0800

    not really like the spaces, yet striptags doesn't work - trying

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+'''Flask module documentation'''
+from flask import Flask, render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    '''method to print'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    '''method to print'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_is(text):
+    '''method to print'''
+    text = text.replace('_', ' ')
+    return f'C {escape(text)}'
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def Python_is(text='is cool'):
+    '''method to print'''
+    text = text.replace('_', ' ')
+    return f'Python {escape(text)}'
+
+
+app.add_url_rule('/python', view_func=Python_is, strict_slashes=False)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def N_is(n):
+    '''method to print'''
+    return f'{escape(n)} is a number'
+
+
+app.add_url_rule('/number/<int:n>', view_func=N_is, strict_slashes=False)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def html_N_is(n):
+    '''method to print'''
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+app.add_url_rule('/number_odd_or_even/<int:n>',
+                 view_func=html_N_is, strict_slashes=False)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    '''method to print'''
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+app.add_url_rule('/number_odd_or_even/<int:n>',
+                 view_func=odd_or_even, strict_slashes=False)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
