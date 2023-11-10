6dc32fe3a570a5aba7d571e2407563144dddfa7b
meisibley <mkong99@yahoo.com>
2023-11-10 09:43:30 -0800

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-'''The flask module documentation'''
 from flask import Flask
 
 
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_HBNB():
-    '''Renders printed text'''
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000)
