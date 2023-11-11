451a5a374390fbfa3cf57b4a207af916aee67c8d
meisibley <mkong99@yahoo.com>
2023-11-11 09:53:42 -0800

    4 is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -32,13 +32,10 @@ def py_text(text="is cool"):
     return "Python {}".format(py_text)
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def num_n(n):
     '''display "n is a number" if n is an int'''
-    if n is int:
-        return "{} is a number".format(n)
-    else:
-        return NULL
+    return "{} is a number".format(n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
