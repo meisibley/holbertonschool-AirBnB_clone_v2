9abf99a9096a3ad8eb77a2eb4cf1a795a6c844e6
meisibley <mkong99@yahoo.com>
2023-11-12 21:35:32 -0800

    7 improve engines

--- models/state.py
+++ models/state.py
@@ -12,15 +12,13 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="delete")
-    if getenv("HBNB_TYPE_STORAGE") == "file":
-        @property
-        def cities(self):
-            """returns the list of City instances"""
-            from models import storage
-            city_list = []
-            for city in list(storage.all(City).values()):
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    cities = relationship("City", backref="state", cascade="all, delete")
+    @property
+    def cities(self):
+        """returns the list of City instances"""
+        from models import storage
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
