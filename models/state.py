752b9423cbf8f73931287b5e3c214aab88a4d999
meisibley <mkong99@yahoo.com>
2023-11-12 21:37:49 -0800

    7 improve engines

--- models/state.py
+++ models/state.py
@@ -13,6 +13,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     cities = relationship("City", backref="state", cascade="all, delete")
+
     @property
     def cities(self):
         """returns the list of City instances"""
