f34a1fd67fe4cdd87d986b63e9f5c0d45ac9f0e4
meisibley <mkong99@yahoo.com>
2023-11-13 23:07:33 -0600

    10 states and state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''using Flask to display HBNB data, list cities by states'''
+'''using Flask to display a HTML page list states, or cities w/ states, or not found'''
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
@@ -13,11 +13,12 @@ def close_session(exc):
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_list():
-    '''using HTML page to display city lists of states'''
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def cities_states(id=None):
+    '''using HTML page to display cities of states'''
     states = storage.all('State')
-    return render_template('8-cities_by_states.html', states=states)
+    return render_template('9-states.html', states=states, id=id)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
