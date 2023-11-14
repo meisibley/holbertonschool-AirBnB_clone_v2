89e5f117d1a19fce2a3a9f374cb564f89024ea9b
meisibley <mkong99@yahoo.com>
2023-11-14 11:03:33 -0600

    10 states and state

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,7 +4,6 @@ states, or cities w/ states, or not found
 '''
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 app = Flask(__name__)
 
@@ -18,9 +17,8 @@ def close_session(exc):
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
 def cities_states(id=None):
-    '''using HTML page to display cities of states'''
-    states = storage.all('State')
-    return render_template('9-states.html', states=states, id=id)
+    '''using HTML page display cities of state'''
+    return render_template('9-states.html', states=storage.all(State), id=id)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
